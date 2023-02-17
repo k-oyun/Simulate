c4167bc020052ede7db11c7a33d199565ed62d25
오윤 <oyun@oyun-ui-MacBookAir.local>
2023-02-17 18:32:25 +0900

    02.17

--- simulate.py
+++ simulate.py
@@ -1,3 +1,6 @@
+import random
+import sys
+
 import pygame
 
 FPS = 30
@@ -73,7 +76,83 @@ def main():
         scoreRect.topleft = (WINDOWWIDTH - 100, 10)
         DISPLAYSURF.blit(scoreSurf, scoreRect)
         DISPLAYSURF.blit(infoSurf, infoRect)
-        # ---->
+
+        # 마우스 클릭 검사
+        checkForQuit()
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos # 마우스 클릭시 해당 포인터의 x,y 좌표를 각각에 저장
+                clickedButton = getButtonClicked(mousex, mousey) # 4개의 버튼중 어떤 버튼을 클릭했는지 color 객체 반환
+
+            # 해당 키 버튼 == 해당 색깔
+            elif event.type == KEYDOWN:
+                if event.key == K_q: # q = 노란색
+                    clickedButton = YELLOW
+
+                elif event.key == K_w: # w = 파란색
+                    clickedButton = BLUE
+
+                elif event.key == K_a: # a = 빨간색
+                    clickedButton = RED
+
+                elif event.key == K_s: # s = 초록색
+                    clickedButton = GREEN
+
+        # 사용자에게 패턴을 보여줌
+        if notwaitingForInput:
+            # 패턴을 자동으로 보여줌.
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pattern.append(random.choice(YELLOW, BLUE, RED, GREEN))
+            for button in pattern:
+                flashButtonAnimation(button)
+                pygame.time.wait(FLASHDELAY)
+            waitingForInput = True  # 사용자의 클릭을 기다림
+
+        # 사용자의 버튼 클릭을 기다림
+        else:
+            if clickedButton and clickedButton == pattern[currentStep]: # 맞는 버튼을 누름
+                flashButtonAnimation(clickedButton) # 플레이어가 누른 버튼을 반짝이게한다.
+                currentStep +=1
+                lastClickTime = time.time() # 마지막 클릭 시간을 현재 시간으로 바꾼다.
+
+                if currentStep == len(pattern):
+                    # 마지맥 패턴의 버튼 클릭
+                    changeBackGroundAnimation():
+                    score += 1
+                    waitingForInput = False
+                    currentStep = 0 # 게임 종료 후 초기화
+
+            elif (clickedButton and clickedButton != pattern[currentStep]) or (currentStep != 0 and time.time() - TIMEOUT > lastClickTime):
+                # time.time() - TIMEOUT > lastClickTime -> 시간초과 검사
+                # 버튼을 잘못 누르거나 시간 초과 시
+                gameOverAnimation()
+                # 새 게임을 위한 변수 초기화
+                pattern = []
+                currentStep = 0
+                waitingForInput = False
+                score = 0
+                pygame.time.wait(1000)
+                changeBackGroundAnimation()
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def checkForQuit():
+    for event in pygame.event.get(QUIT):
+        terminate() # QUIT 이벤트가 발생 시 종료
+    for event in pygame.event.get(KEYUP):
+        if event.key == k_ESCAPE:
+            terminate() # KEYUP 이벤트가 Esc면 종료
+        pygame.event.post(event)
+
+
+
+
 
 
 
